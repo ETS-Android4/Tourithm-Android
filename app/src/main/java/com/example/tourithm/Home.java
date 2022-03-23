c10fdabab635dd126268e2a1eb2182d2aab33b07
1DOX <veritas0818@gmail.com>
2022-03-23 15:19:01 +0900

    First Commit.

--- app/src/main/java/com/example/tourithm/Home.java
+++ app/src/main/java/com/example/tourithm/Home.java
@@ -10,6 +10,6 @@ public class Home extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.home);
+        setContentView(R.layout.homescreen);
     }
 }
