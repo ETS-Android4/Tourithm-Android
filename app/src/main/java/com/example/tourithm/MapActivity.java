2c5d63406cfa3a2f42047b52cd6b1f7e9236a430
hyoeun <gjf120@naver.com>
2022-04-13 19:30:44 +0900

    HANYO _ MAP ( 마커 클릭시 정보 뜸, 전화 기능, 스크랩은 손 더 대야됨 )

--- app/src/main/java/com/example/tourithm/MapActivity.java
+++ app/src/main/java/com/example/tourithm/MapActivity.java
@@ -1,10 +1,13 @@
 package com.example.tourithm;
 
 import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,11 +19,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 
+import com.naver.maps.geometry.Coord;
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraAnimation;
 import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
@@ -38,7 +43,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+import java.util.Map;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -51,7 +56,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     };
 
     LatLng[] latlng = new LatLng[100];
-    String[][] infodata = new String[100][5];
+    String[][] infodata = new String[100][6];
     LinearLayout info;
 
     @Override
@@ -77,9 +82,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         locationSource = new FusedLocationSource(this,LOCATION_PERMISSION_REQUEST_CODE);
 
     }
-
-
-
 
     // DB - 데이터 불러오기
     class Select_local_Request extends AsyncTask<String, Integer, String> {
@@ -130,18 +132,19 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     // double -> String
                     String st_latitude = Double.toString(latitude);
                     String st_longitude = Double.toString(longitude);
-                    // 데이터 배열에 넣기
+                    // 데이터 배열에 넣기 ( 위도, 경도, 이름, 주소, 전번, 스크랩 )
                     infodata[index][0] = st_latitude;
                     infodata[index][1] = st_longitude;
                     infodata[index][2] = name;
                     infodata[index][3] = road;
                     infodata[index][4] = tel;
+                    infodata[index][5] = "false";
 
                     // 마커 찍기
                     addInfo(latlng[index]);
 
                     Log.d("[latlng]", latlng[index].toString());
-                    for(int i = 0; i < 5; i++){
+                    for(int i = 0; i < 6; i++){
                         Log.d("[infodata]", infodata[index][i]);
                     }
 
@@ -178,15 +181,14 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         // 권한 확인
         ActivityCompat.requestPermissions(this, PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE);
 
-        // 지도를 클릭하면 정보 창을 닫음
-        naverMap.setOnMapClickListener((coord, point) -> {
-            info.setVisibility(View.GONE);
-        });
-
     }
 
     // 마커 관련
     public void addInfo(LatLng latLng) {
+
+        ImageButton telbtn = (ImageButton) findViewById(R.id.mark_tel_bt);
+        ImageButton scrapbtn = (ImageButton) findViewById(R.id.mark_scrap);
+        ImageButton findway = (ImageButton) findViewById(R.id.mark_find_way);
 
         TextView title = (TextView) findViewById(R.id.mark_title);
         TextView addr = (TextView) findViewById(R.id.mark_addr);
@@ -201,7 +203,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         marker.setWidth(70);
         marker.setHeight(100);
 
-        //marker.setOnClickListener(this);
         // 마커 모양
         marker.setIcon(OverlayImage.fromResource(R.drawable.place_mark));
 
@@ -213,21 +214,27 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
                     // 클릭한 좌표
                     LatLng lat = marker.getPosition().toLatLng();
-                    // System.out.println("lat : " + lat);
 
                     // 배열 좌표
                     for(int i = 0; i < 100; i++){
                         Double aa = Double.parseDouble(infodata[i][0]);
                         Double ab = Double.parseDouble(infodata[i][1]);
-                        LatLng c = new LatLng(aa, ab);
+                        LatLng c = new LatLng(aa, ab); // 배열에 있는 좌표를 lanlng에 넣기
 
                         // 두 좌표의 값이 같은지
                         boolean B = lat.equals(c);
                         if(B == true){ // 같다면
                             // 사진, 코로나 수 어캄??
-                            title.setText(infodata[i][2]);
-                            addr.setText(infodata[i][3]);
-                            tel.setText(infodata[i][4]);
+                            String titl = infodata[i][2];
+                            String adr = infodata[i][3];
+                            String call = infodata[i][4];
+                            String scrap = infodata[i][5];
+
+                            System.out.println("titl : "+titl+"/ adr : "+adr+"/ call : "+call+"/scrap"+scrap);
+
+                            title.setText(titl);
+                            addr.setText(adr);
+                            tel.setText(call);
                             findViewById(R.id.map_mark_info).setVisibility(View.VISIBLE); // 정보창 보이게
 
                             // 카메라 이동 애니메이션
@@ -236,15 +243,61 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                                     .animate(CameraAnimation.Fly, 1000);
                             naverMap.moveCamera(cameraUpdate);
 
+                            String finalCall = "tel:" + call;
+                            //Boolean ima = Boolean.parseBoolean(scrap);
+
+                            // 전화 걸기 버튼
+                            telbtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    startActivity(new Intent("android.intent.action.DIAL", Uri.parse(finalCall)));
+                                }
+                            });
+
+                            // 스크랩 버튼
+                            scrapbtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    if(scrap.equals("false")){
+
+                                        scrapbtn.setImageResource(R.drawable.star_color);
+                                        Intent intent = new Intent(getApplicationContext(), Scrap.class);
+                                        intent.putExtra("latlng", c); // 좌표 전송
+                                        scrap.replace("false","true");
+                                        System.out.println(scrap);
+                                    }else if(scrap.equals("true")) {
+                                        scrapbtn.setImageResource(R.drawable.star);
+                                        scrap.replace("true","false");
+                                        System.out.println(scrap);
+                                    }
+
+                                }
+                            });
+
+                            // 길찾기 버튼
+                            findway.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    /*
+                                    Intent intent = new Intent(getApplicationContext(), ?.class);
+                                    intent.putExtra("latlng", c); // 좌표 전송
+                                    startActivity(intent);*/
+                                }
+                            });
 
                         }
-
                     }
                     return true;
                 }
                 return false;
             }
         });
+
+        // 지도를 클릭하면 정보 창을 닫음
+        naverMap.setOnMapClickListener((coord, point) -> {
+            info.setVisibility(View.GONE);
+        });
+
     }
 
     // 현재위치 권한 확인
--- app/src/main/java/com/example/tourithm/PlaceData.java
+++ app/src/main/java/com/example/tourithm/PlaceData.java
@@ -20,7 +20,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
-import Adapter.PlaceData_Adapter;
+import com.example.tourithm.PlaceData_Adapter;
 
 public class PlaceData extends AppCompatActivity {
 
